22d00021f3fb730f8cd46e4ed17ef436d4fc1f2f
Otavio <estudosprogotavio@gmail.com>
2025-02-19 15:33:03 -0300

    Commit  iniciais

---
+++ src/test/java/dev/Zerphyis/planinha/PlaninhaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.planinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaninhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
