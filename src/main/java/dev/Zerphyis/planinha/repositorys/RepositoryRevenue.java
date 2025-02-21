a2875dd52ff4569e38e03c5ea5a9f28d13c9953b
Otavio <estudosprogotavio@gmail.com>
2025-02-21 00:50:04 -0300

    Atualizando RepositoryRevenue

--- src/main/java/dev/Zerphyis/planinha/repositorys/RepositoryRevenue.java
+++ src/main/java/dev/Zerphyis/planinha/repositorys/RepositoryRevenue.java
@@ -1,13 +1,22 @@
 package dev.Zerphyis.planinha.repositorys;
 
+import dev.Zerphyis.planinha.entity.expenses.Expenses;
 import dev.Zerphyis.planinha.entity.revunue.Revenue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RepositoryRevenue extends JpaRepository<Revenue, Long > {
     Optional<Revenue> findByDescriptionAndDataBetween(String description, LocalDate startDate, LocalDate endDate);
+
+    List<Revenue> findByDescriptionContaining(String descricao);
+
+    List<Revenue> findByDataBetween(LocalDate startDate, LocalDate endDate);
+
 }
