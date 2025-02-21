59e7c7263957ba4d59de5792cec4de3e6dfd7bc4
Otavio <estudosprogotavio@gmail.com>
2025-02-21 00:51:20 -0300

    Atualizando ServiceRevunue

--- src/main/java/dev/Zerphyis/planinha/services/ServiceExpenses.java
+++ src/main/java/dev/Zerphyis/planinha/services/ServiceExpenses.java
@@ -1,17 +1,19 @@
 package dev.Zerphyis.planinha.services;
 
+import dev.Zerphyis.planinha.entity.expenses.DataExpenses;
 import dev.Zerphyis.planinha.entity.expenses.Expenses;
-import dev.Zerphyis.planinha.entity.revunue.DataRevunue;
-import dev.Zerphyis.planinha.entity.revunue.Revenue;
 import dev.Zerphyis.planinha.repositorys.RepositoryExpenses;
 import jakarta.persistence.Transient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceExpenses {
@@ -19,7 +21,7 @@ public class ServiceExpenses {
     RepositoryExpenses repository;
 
     @Transient
-    public Expenses registerExpenses(DataRevunue data){
+    public Expenses registerExpenses(DataExpenses data){
         YearMonth monthYear = YearMonth.from(data.data());
         LocalDate startOfMonth = monthYear.atDay(1);
         LocalDate endOfMonth = monthYear.atEndOfMonth();
@@ -28,10 +30,10 @@ public class ServiceExpenses {
                 data.description(), startOfMonth, endOfMonth);
 
         if (existingExpenses.isPresent() && YearMonth.from(existingExpenses.get().getData()).equals(monthYear)) {
-            throw new IllegalArgumentException("Já existe uma receita com essa descrição para este mês específico.");
+            throw new IllegalArgumentException("Já existe uma Despesa com essa descrição para este mês específico.");
         }
 
-        var newExpenses = new Expenses(data);
+        Expenses newExpenses = new Expenses(data);
         return repository.save(newExpenses);
     }
 
@@ -43,20 +45,34 @@ public class ServiceExpenses {
         return repository.findById(id);
     }
 
+    public List<Expenses> listByDescription(String descricao) {
+        return repository.findByDescriptionContaining(descricao);
+    }
+
     @Transient
-    public Expenses updateExpenses(Long id,DataRevunue data){
+    public Expenses updateExpenses(Long id,DataExpenses data){
         Optional<Expenses> idExpenses =  repository.findById(id);
         if (idExpenses.isPresent()) {
             Expenses updateExpenses = idExpenses.get();
             updateExpenses.setDescription(data.description());
             updateExpenses.setValue(data.value());
             updateExpenses.setData(data.data());
+            updateExpenses.setType(data.type());
 
             return repository.save(updateExpenses);
         } else {
             throw new RuntimeException("Receita não encontrado com o ID: " + id);
         }
     }
+
+
+    public List<Expenses> listByMonth(int year, int month) {
+        YearMonth yearMonth = YearMonth.of(year, month);
+        LocalDate startOfMonth = yearMonth.atDay(1);
+        LocalDate endOfMonth = yearMonth.atEndOfMonth();
+        return repository.findByDataBetween(startOfMonth, endOfMonth);
+    }
+
 
     @Transient
     public void DeleteExpenses(Long id){
