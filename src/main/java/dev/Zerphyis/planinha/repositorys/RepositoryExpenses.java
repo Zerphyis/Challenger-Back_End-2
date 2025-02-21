7bd7bcf911e98ec35bc9db770d0c14d0ddc4b7ea
Otavio <estudosprogotavio@gmail.com>
2025-02-21 00:50:41 -0300

    Atualizando RepositoryExpenses

--- src/main/java/dev/Zerphyis/planinha/repositorys/RepositoryExpenses.java
+++ src/main/java/dev/Zerphyis/planinha/repositorys/RepositoryExpenses.java
@@ -1,13 +1,20 @@
 package dev.Zerphyis.planinha.repositorys;
 
 import dev.Zerphyis.planinha.entity.expenses.Expenses;
-import dev.Zerphyis.planinha.entity.revunue.Revenue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface RepositoryExpenses  extends JpaRepository<Expenses , Long> {
     Optional<Expenses> findByDescriptionAndDataBetween(String description, LocalDate startDate, LocalDate endDate);
+
+
+    List<Expenses> findByDescriptionContaining(String descricao);
+
+    List<Expenses> findByDataBetween(LocalDate startDate, LocalDate endDate);
 }
